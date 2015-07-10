587daa1a816c2bf2d7e1d3d21d0be1b6a045adc3
Andrej Dragojevic <andrejdr.93@gmail.com>
2015-07-10 19:21:27 +0200

    User signup with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  before_save {self.email.downcase!}
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, :format => {with: VALID_EMAIL_REGEX}, length: {maximum: 255}, uniqueness: true
+  validates :password, presence: true, length: {minimum: 8}, allow_blank: true
+  has_secure_password
 end
