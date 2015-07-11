02136402559d09da65f9a617d5969a9a60c4f6f2
Andrej Dragojevic <andrejdr.93@gmail.com>
2015-07-11 19:49:57 +0200

    Event date and time validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  before_save :format_time
+  before_create :format_time
 
   # Associations
 
@@ -14,9 +14,28 @@ class Event < ActiveRecord::Base
   validates :location, presence: true, length: {maximum: 30}
   validates :event_date, presence: true
   validates :event_time, presence: true
+  validate :event_time_check
+  validate :event_date_check
+
 
   def format_time
     date = Date.today
     self.event_time = self.event_time.to_datetime.change(day: date.day, month: date.month, year: date.year).to_time
+  end
+
+  def event_date_check
+    event_date < Date.today ? errors.add(:event_date, "can't be in the past") : true if !event_date.nil?
+  end
+
+  def event_time_check
+    if event_date == Date.today && !event_date.nil? && !event_time.nil?
+      if event_time.hour < Time.now.hour
+        errors.add(:event_time, "can't be in the past")
+      elsif event_time.hour == Time.now.hour && event_time.min > Time.now.min
+        errors.add(:event_time, "too soon to be organized")
+      else
+        true
+      end
+    end
   end
 end
