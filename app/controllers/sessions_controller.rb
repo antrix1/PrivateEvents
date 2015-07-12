d42b653e9be0d2bf3cf995a55d4122479b5607a7
Andrej Dragojevic <andrejdr.93@gmail.com>
2015-07-12 23:40:06 +0200

    Downcase user email when checking login credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
       flash[:success] = "Successfully logged in"
