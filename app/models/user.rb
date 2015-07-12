08b249f990f8f0f80454d88ecf1e9b5a0be5c2a6
Andrej Dragojevic <andrejdr.93@gmail.com>
2015-07-12 22:26:30 +0200

    Added dependent destroy to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
 
   # Associations
 
-  has_many :created_events, class_name: Event, foreign_key: :creator_id
-  has_many :invitations, foreign_key: :attendee_id
+  has_many :created_events, class_name: Event, foreign_key: :creator_id, :dependent => :destroy
+  has_many :invitations, foreign_key: :attendee_id, :dependent => :destroy
   has_many :attended_events, through: :invitations, source: :event
 
 ###########################################################
